1bd4fb393abd05a23cfcc9b4857c4c17f25235d9
Funmi <folowookere@yahoo.com>
2018-09-04 16:33:19 -0400

    Start of Cloudy Day solution

---
+++ hackerrank/cloudyday.cs
@@ -0,0 +1,29 @@
+using System;
+namespace Mosh5Exercises
+{
+    public class cloudyday
+    {
+        public cloudyday()
+        {
+
+            //Cloud clouds = 0;
+            //City cities = new City();
+            
+            //create new lists
+
+            List<oneCloud> OneCloudList = new List<oneCloud>();
+            List<anyCloud> AnyCloudList = new List<anyCloud>();
+            List<citiesCovered> CitiesList = new List<citiesCovered>();
+
+            // example of adding items to the list
+            OneCloudList.Add();
+            AnyCloudList.Add();
+            CitiesList.Add();
+
+            // example of removing items form the list
+            CitiesList.Remove();
+
+
+        }
+    }
+}
