ed89c66f09444d934bca3a90bf7ac339588ab456
Funmi <folowookere@yahoo.com>
2018-09-05 23:04:20 -0700

    Started fresh with figuring out data structure

--- hackerrank/cloudyday.cs
+++ hackerrank/cloudyday.cs
@@ -1,27 +1,15 @@
 using System;
-namespace Mosh5Exercises
+namespace Application
 {
-    public class cloudyday
+    public class CloudyDay
     {
-        public cloudyday()
+        public CloudyDay()
         {
 
-            //Cloud clouds = 0;
-            //City cities = new City();
-            
-            //create new lists
-
-            List<oneCloud> OneCloudList = new List<oneCloud>();
-            List<anyCloud> AnyCloudList = new List<anyCloud>();
-            List<citiesCovered> CitiesList = new List<citiesCovered>();
-
-            // example of adding items to the list
-            OneCloudList.Add();
-            AnyCloudList.Add();
-            CitiesList.Add();
-
-            // example of removing items form the list
-            CitiesList.Remove();
+            List<oneCloud[]> OneCloudList = new List<oneCloud[]>();
+            {
+                new int[] {}
+            }
 
 
         }
