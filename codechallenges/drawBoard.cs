bc064a89d5bc6a7e399c641c78dae9f22e2dcde7
Funmi <folowookere@yahoo.com>
2019-01-20 23:50:46 -1000

    Continuing using command line to draw a board

---
+++ codechallenges/drawBoard.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace CodeChallenges2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Calling method
+            DrawBoard();
+
+        }
+
+        public static void DrawBoard()
+        {
+            {
+                int rows = 2;
+                for (int i = 1; i <= rows; i++)
+                {
+
+                    Console.WriteLine(" | | ");
+
+                    Console.WriteLine("-+-+-");
+
+                }
+
+                    Console.WriteLine(" | | ");
+
+                }
+            }
+        }
+    }
